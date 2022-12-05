00b77c5e36434487f2d7603e97b314125171c406
Pawel Gozdzikowski <pgozdzikowski@gmail.com>
2022-12-05 08:26:50 +0100

    Day5

---
+++ src/main/java/pl/gozdzikowski/pawel/adventofcode/day5/SupplyStacks.java
@@ -0,0 +1,75 @@
+package pl.gozdzikowski.pawel.adventofcode.day5;
+
+import java.util.*;
+import java.util.function.Consumer;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class SupplyStacks {
+    private static Pattern movePattern = Pattern.compile("move (\\d+) from (\\d+) to (\\d+)");
+    private List<Stack<String>> supplyStacks;
+
+    public SupplyStacks(List<Stack<String>> supplyStacks) {
+        this.supplyStacks = supplyStacks;
+    }
+
+    public String getMessageFromStacksChangeOrder(String moves) {
+        String[] movesArray = moves.split("\\n");
+
+        Arrays.stream(movesArray).forEach((move) -> executeMove(move, this::onlyOneItemAtOnce));
+
+        return formatMessageFromStacks();
+    }
+
+    public String getMessageFromStacksChangeOrderWithLeaveOrder(String moves) {
+        String[] movesArray = moves.split("\\n");
+
+        Arrays.stream(movesArray).forEach((move) -> executeMove(move, this::manyItemsAtOnce));
+
+        return formatMessageFromStacks();
+    }
+
+    private void executeMove(String move, Consumer<ActionDescription> consumer) {
+        Matcher matcher = movePattern.matcher(move);
+        matcher.find();
+        int numOfItems = Integer.parseInt(matcher.group(1));
+        int sourceStack = Integer.parseInt(matcher.group(2)) - 1;
+        int destStack = Integer.parseInt(matcher.group(3)) - 1;
+        consumer.accept(new ActionDescription(numOfItems, sourceStack, destStack));
+    }
+
+    void onlyOneItemAtOnce(ActionDescription action) {
+        int itemsLefts = action.numOfItems;
+        while(itemsLefts > 0) {
+            String poppedCreate = supplyStacks.get(action.sourceStack()).pop();
+            supplyStacks.get(action.destStack()).push(poppedCreate);
+            itemsLefts--;
+        }
+    }
+
+    void manyItemsAtOnce(ActionDescription action) {
+        Deque<String> takenCrafts = new LinkedList<>();
+        int itemsLeft = action.numOfItems;
+        while(itemsLeft > 0) {
+            String poppedCreate = supplyStacks.get(action.sourceStack()).pop();
+            takenCrafts.offerFirst(poppedCreate);
+            itemsLeft--;
+        }
+        supplyStacks.get(action.destStack()).addAll(takenCrafts);
+    }
+
+    private String formatMessageFromStacks() {
+        return supplyStacks.stream()
+                .filter((s) -> !s.isEmpty())
+                .map(Stack::pop)
+                .reduce("", (acc, next) -> acc + next);
+    }
+
+    record ActionDescription(
+            int numOfItems,
+            int sourceStack,
+            int destStack
+    ) {
+
+    }
+}
---
+++ src/test/groovy/pl/gozdzikowski/pawel/adventofcode/day5/Day5Spec.groovy
@@ -0,0 +1,87 @@
+package pl.gozdzikowski.pawel.adventofcode.day5
+
+import pl.gozdzikowski.pawel.adventofcode.shared.input.FileInput
+import pl.gozdzikowski.pawel.adventofcode.shared.input.Input
+import spock.lang.Specification
+
+class Day5Spec extends Specification {
+
+    def SUPPLY_STACK_DEF = [
+            ['Z', 'N'] as Stack<String>,
+            ['M', 'C', 'D'] as Stack<String>,
+            ['P'] as Stack<String>
+    ]
+
+    def MOVES = """move 1 from 2 to 1
+move 3 from 1 to 3
+move 2 from 2 to 1
+move 1 from 1 to 2
+"""
+
+
+    def 'should create message from test supply stacks with change order'() {
+        given:
+        SupplyStacks supplyStacks = new SupplyStacks(SUPPLY_STACK_DEF)
+        when:
+        def message = supplyStacks.getMessageFromStacksChangeOrder(MOVES)
+        then:
+        message == "CMZ"
+    }
+
+    def 'task solution 1'() {
+        given:
+        def supplyStacksDef = [
+                ['F', 'C', 'J', 'P', 'H', 'T', 'W'] as Stack<String>,
+                ['G', 'R', 'V', 'F', 'Z', 'J', 'B', 'H'] as Stack<String>,
+                ['H', 'P', 'T', 'R'] as Stack<String>,
+                ['Z', 'S', 'N', 'P', 'H', 'T'] as Stack<String>,
+                ['N', 'V', 'F', 'Z', 'H', 'J', 'C', 'D'] as Stack<String>,
+                ['P', 'M', 'G', 'F', 'W', 'D', 'Z'] as Stack<String>,
+                ['M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P'] as Stack<String>,
+                ['N', 'D', 'S'] as Stack<String>,
+                ['D', 'Z', 'S', 'F', 'M'] as Stack<String>
+        ]
+
+        SupplyStacks supplyStacks = new SupplyStacks(supplyStacksDef)
+        Input input = new FileInput("day5.txt")
+        def moves = input.getContent().split("\\n\\n")
+        when:
+        def message = supplyStacks.getMessageFromStacksChangeOrder(moves[1])
+        then:
+        message == "SPFMVDTZT"
+    }
+
+    def 'should create message from test supply stacks'() {
+        given:
+        SupplyStacks supplyStacks = new SupplyStacks(SUPPLY_STACK_DEF)
+        when:
+        def message = supplyStacks.getMessageFromStacksChangeOrderWithLeaveOrder(MOVES)
+        then:
+        message == "MCD"
+    }
+
+    def 'task solution 2'() {
+        given:
+        def supplyStacksDef = [
+                ['F', 'C', 'J', 'P', 'H', 'T', 'W'] as Stack<String>,
+                ['G', 'R', 'V', 'F', 'Z', 'J', 'B', 'H'] as Stack<String>,
+                ['H', 'P', 'T', 'R'] as Stack<String>,
+                ['Z', 'S', 'N', 'P', 'H', 'T'] as Stack<String>,
+                ['N', 'V', 'F', 'Z', 'H', 'J', 'C', 'D'] as Stack<String>,
+                ['P', 'M', 'G', 'F', 'W', 'D', 'Z'] as Stack<String>,
+                ['M', 'V', 'Z', 'W', 'S', 'J', 'D', 'P'] as Stack<String>,
+                ['N', 'D', 'S'] as Stack<String>,
+                ['D', 'Z', 'S', 'F', 'M'] as Stack<String>
+        ]
+
+        SupplyStacks supplyStacks = new SupplyStacks(supplyStacksDef)
+        Input input = new FileInput("day5.txt")
+        def moves = input.getContent().split("\\n\\n")
+        when:
+        def message = supplyStacks.getMessageFromStacksChangeOrderWithLeaveOrder(moves[1])
+        then:
+        message == "ZFSJBPRFP"
+    }
+
+
+}
