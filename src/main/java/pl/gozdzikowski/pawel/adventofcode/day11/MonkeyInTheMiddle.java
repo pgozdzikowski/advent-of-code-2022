b514d391d8e4b2e500cf4247732d61841d3856ed
Pawel Gozdzikowski <pgozdzikowski@gmail.com>
2022-12-12 06:42:55 +0100

    Second task day 11

--- src/main/java/pl/gozdzikowski/pawel/adventofcode/day11/MonkeyInTheMiddle.java
+++ src/main/java/pl/gozdzikowski/pawel/adventofcode/day11/MonkeyInTheMiddle.java
@@ -1,75 +1,113 @@
 package pl.gozdzikowski.pawel.adventofcode.day11;
 
-import java.util.Comparator;
-import java.util.Deque;
-import java.util.LinkedList;
-import java.util.List;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.util.*;
 import java.util.function.Function;
 
 public class MonkeyInTheMiddle {
 
+    private Monkey[] monkeys;
 
-    private List<Monkey> monkeys;
-
-    public MonkeyInTheMiddle(List<Monkey> monkeys) {
+    public MonkeyInTheMiddle(Monkey[] monkeys) {
         this.monkeys = monkeys;
     }
 
-    public long findProductOfMultiplyOfMostActiveMonkeys(int roundsNum, Function<Long, Long> worryFunction) {
-        while (roundsNum > 0) {
-            for (Monkey monkey : monkeys) {
-                while (monkey.hasAnyItem()) {
-                    ItemTransfer itemTransfer = monkey.takeOneItemAndThrow(worryFunction);
-                    monkeys.get(itemTransfer.destMonkey).addItem(itemTransfer.worryLevel());
-                }
-            }
-            roundsNum--;
+    public long findProductOfMultiplyOfMostActiveMonkeys(int iterations, Map<Integer, List<Long>> initialState) {
+        List<Item> items = initialState.entrySet().stream().flatMap(
+                (entry) -> entry.getValue().stream().map((itemValue) -> new Item(itemValue, entry.getKey(), monkeys.length))
+        ).toList();
+
+        for (int i = 0; i < items.size(); ++i) {
+            System.out.println("Execute operations fro item" +i);
+            items.get(i).executeIterations(iterations, monkeys);
         }
-        return monkeys.stream().sorted(Comparator.comparing(Monkey::getNumberOfItemsInspected).reversed())
+
+        long[] summedPosses = items.stream()
+                .map(Item::getHowManyByMonkey)
+                .reduce(new long[monkeys.length], this::reduceItems);
+
+        return Arrays.stream(summedPosses)
+                .boxed()
+                .sorted(Comparator.reverseOrder())
                 .limit(2)
-                .map(Monkey::getNumberOfItemsInspected)
+                .mapToLong(Long::valueOf)
                 .reduce(1, (acc, item) -> acc * item);
     }
 
-    static class Monkey {
-        private Deque<Long> items;
-        private Function<Long, Long> operation;
-        private Function<Long, Integer> forwardCondtion;
-        private int numberOfItemsInspected = 0;
+    private long[] reduceItems(long[] acc, long[] possessed) {
+        for (int i = 0; i < acc.length; ++i) {
+            acc[i] += possessed[i];
+        }
+        return acc;
+    }
 
-        public Monkey(List<Long> items, Function<Long, Long> operation, Function<Long, Integer> forwardCondition) {
-            this.items = new LinkedList<>(items);
+    static class Monkey {
+        private MonkeyOperation operation;
+        private Function<Item, Integer> forwardCondtion;
+
+        public Monkey(MonkeyOperation operation, Function<Item, Integer> forwardCondition) {
             this.operation = operation;
             this.forwardCondtion = forwardCondition;
         }
 
-        public boolean hasAnyItem() {
-            return !items.isEmpty();
-        }
 
-        public ItemTransfer takeOneItemAndThrow(Function<Long, Long> worryFunction) {
-            Long item = items.poll();
-            Long worryLevelAfterApply = operation.apply(item);
-            numberOfItemsInspected++;
-            long worryLevelAfterDivision = worryFunction.apply(worryLevelAfterApply);
-            return new ItemTransfer(worryLevelAfterDivision, forwardCondtion.apply(worryLevelAfterDivision));
-        }
 
-        public void addItem(Long item) {
-            items.add(item);
-        }
-
-        public int getNumberOfItemsInspected() {
-            return numberOfItemsInspected;
+        public Function<Item, Integer> getForwardCondtion() {
+            return forwardCondtion;
         }
     }
 
-    record ItemTransfer(
-            long worryLevel,
-            int destMonkey
+    class Item {
+        private long[] howManyByMonkey;
+        private int currentMonkey;
+
+        private List<MonkeyOperation> monkeyOperations = new LinkedList<>();
+
+        public Item(Long item, int currentMonkey,  int sizeOfMonkeys) {
+            this.currentMonkey = currentMonkey;
+            this.howManyByMonkey = new long[sizeOfMonkeys];
+            monkeyOperations.add(new MonkeyOperation(item, ArithmeticOperation.NOOP));
+        }
+
+        void executeIterations(int iterations, Monkey[] monkeys) {
+            while (iterations > 0) {
+                int prevMonkey = currentMonkey;
+                while(currentMonkey >= prevMonkey) {
+                    howManyByMonkey[currentMonkey]++;
+                    Monkey monkey = monkeys[currentMonkey];
+                    monkeyOperations.add(monkey.operation);
+                    prevMonkey = currentMonkey;
+                    currentMonkey = monkey.getForwardCondtion().apply(this);
+                }
+                --iterations;
+            }
+        }
+
+        public long[] getHowManyByMonkey() {
+            return howManyByMonkey;
+        }
+    }
+
+    record MonkeyOperation(
+            long secondOperand,
+            ArithmeticOperation operation
     ) {}
 
-    public static boolean isDivisible(long num, int by) {
-        return num % by == 0;
+    enum ArithmeticOperation {
+        ADD, MUL, POW, NOOP
+    }
+
+    public static boolean isDivisible(Item item, int by) {
+        BigInteger result = BigInteger.ZERO;
+        for (MonkeyOperation monkeyOperation : item.monkeyOperations) {
+            switch (monkeyOperation.operation) {
+                case MUL -> result = result.multiply(BigInteger.valueOf(monkeyOperation.secondOperand() % by));
+                case ADD -> result = result.add(BigInteger.valueOf(monkeyOperation.secondOperand() % by));
+                case POW -> result = (result.multiply(result.remainder(BigInteger.valueOf(by))));
+                case NOOP -> result = BigInteger.valueOf(monkeyOperation.secondOperand() % by);
+            }
+        }
+        return result.remainder(BigInteger.valueOf(by)).compareTo(BigInteger.ZERO) == 0;
     }
 }
--- src/test/groovy/pl/gozdzikowski/pawel/adventofcode/day11/Day11Spec.groovy
+++ src/test/groovy/pl/gozdzikowski/pawel/adventofcode/day11/Day11Spec.groovy
@@ -1,5 +1,6 @@
 package pl.gozdzikowski.pawel.adventofcode.day11
 
+import spock.lang.Ignore
 import spock.lang.Shared
 import spock.lang.Specification
 
@@ -10,81 +11,99 @@ class Day11Spec extends Specification {
     @Shared
     def sampleConfig = [
             new Monkey(
-                    [79L, 98L],
-                    { old -> old * 19 },
-                    { calc -> isDivisible(calc as long, 23) ? 2 : 3 }
+                    new MonkeyOperation(19, ArithmeticOperation.MUL),
+                    { calc -> isDivisible(calc, 23) ? 2 : 3 }
             ),
             new Monkey(
-                    [54L, 65L, 75L, 74L],
-                    { old -> old + 6 },
-                    { calc -> isDivisible(calc as long, 19) ? 2 : 0 }
+                    new MonkeyInTheMiddle.MonkeyOperation(6, ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 19) ? 2 : 0 }
             ),
             new Monkey(
-                    [79L, 60L, 97L],
-                    { old -> old * old },
-                    { calc -> isDivisible(calc as long, 13) ? 1 : 3 }
+                    new MonkeyOperation(0, ArithmeticOperation.POW),
+                    { calc -> isDivisible(calc, 13) ? 1 : 3 }
             ),
             new Monkey(
-                    [74L],
-                    { old -> old + 3 },
-                    { calc -> isDivisible(calc as long, 17) ? 0 : 1 }
+                    new MonkeyInTheMiddle.MonkeyOperation(3, ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 17) ? 0 : 1 }
             )
 
     ]
 
-    @Shared
+    Map<Integer, List<Long>> initialStateOfSample = [
+            0: [79L, 98L],
+            1: [54L, 65L, 75L, 74L],
+            2: [79L, 60L, 97L],
+            3: [74L]
+    ]
+
     def taskConfig = [
             new Monkey(
-                    [75L, 63L],
-                    { old -> old * 3 },
-                    { calc -> isDivisible(calc as long, 11) ? 7 : 2 }
+                    new MonkeyInTheMiddle.MonkeyOperation(3, ArithmeticOperation.MUL),
+                    { calc -> isDivisible(calc , 11) ? 7 : 2 }
             ),
             new Monkey(
-                    [65L, 79L, 98L, 77L, 56L, 54L, 83L, 94L],
-                    { old -> old + 3 },
-                    { calc -> isDivisible(calc as long, 2) ? 2 : 0 }
+                    new MonkeyInTheMiddle.MonkeyOperation(3,ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 2) ? 2 : 0 }
             ),
             new Monkey(
-                    [66L],
-                    { old -> old + 5 },
-                    { calc -> isDivisible(calc as long, 5) ? 7 : 5 }
+                    new MonkeyInTheMiddle.MonkeyOperation(5, ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 5) ? 7 : 5 }
             ),
             new Monkey(
-                    [51L, 89L, 90L],
-                    { old -> old * 19 },
-                    { calc -> isDivisible(calc as long, 7) ? 6 : 4 }
+                    new MonkeyInTheMiddle.MonkeyOperation(19, ArithmeticOperation.MUL),
+                    { calc -> isDivisible(calc, 7) ? 6 : 4 }
             ),
             new Monkey(
-                    [75L, 94L, 66L, 90L, 77L, 82L, 61L],
-                    { old -> old + 1 },
-                    { calc -> isDivisible(calc as long, 17) ? 6 : 1 }
+                    new MonkeyInTheMiddle.MonkeyOperation(1, ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 17) ? 6 : 1 }
             ),
             new Monkey(
-                    [53L, 76L, 59L, 92L, 95L],
-                    { old -> old + 2 },
-                    { calc -> isDivisible(calc as long, 19) ? 4 : 3 }
+                    new MonkeyInTheMiddle.MonkeyOperation(2, ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 19) ? 4 : 3 }
             ),
             new Monkey(
-                    [81L, 61L, 75L, 89L, 70L, 92L],
-                    { old -> old * old },
-                    { calc -> isDivisible(calc as long, 3) ? 0 : 1 }
+
+                    new MonkeyInTheMiddle.MonkeyOperation(0, ArithmeticOperation.POW),
+                    { calc -> isDivisible(calc, 3) ? 0 : 1 }
             ),
             new Monkey(
-                    [81L, 86L, 62L, 87L],
-                    { old -> old + 8 },
-                    { calc -> isDivisible(calc as long, 13) ? 3 : 5 }
+                    new MonkeyOperation(8, ArithmeticOperation.ADD),
+                    { calc -> isDivisible(calc, 13) ? 3 : 5 }
             ),
+    ]
+
+    def initialStateOfMonkeyTask = [
+            0 : [75L, 63L],
+            1: [65L, 79L, 98L, 77L, 56L, 54L, 83L, 94L] ,
+            2: [66L],
+            3:  [51L, 89L, 90L],
+            4:  [75L, 94L, 66L, 90L, 77L, 82L, 61L],
+            5:  [53L, 76L, 59L, 92L, 95L],
+            6:  [81L, 61L, 75L, 89L, 70L, 92L],
+            7:  [81L, 86L, 62L, 87L],
     ]
 
     def 'should simulate and find product of multiply of most active monkeys'() {
         given:
-        MonkeyInTheMiddle monkeyInTheMiddle = new MonkeyInTheMiddle(config);
+        MonkeyInTheMiddle monkeyInTheMiddle = new MonkeyInTheMiddle(sampleConfig as Monkey[])
         expect:
-        result == monkeyInTheMiddle.findProductOfMultiplyOfMostActiveMonkeys(20, { (long) (it / 3) })
+        result == monkeyInTheMiddle.findProductOfMultiplyOfMostActiveMonkeys(iterations, initialStateOfSample)
         where:
-        config       || result
-        sampleConfig || 10605L
-        taskConfig   || 62491
+        result         || iterations
+        99L * 103      || 20
+        5204L * 5192   || 1000
+        52166L * 52013 || 10000
     }
 
+
+    @Ignore
+    def 'task results'() {
+        given:
+        MonkeyInTheMiddle monkeyInTheMiddle = new MonkeyInTheMiddle(taskConfig as Monkey[])
+        expect:
+        result == monkeyInTheMiddle.findProductOfMultiplyOfMostActiveMonkeys(iterations, initialStateOfMonkeyTask)
+        where:
+        result         || iterations
+        2713310158 || 10000
+    }
 }
